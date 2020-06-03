da5a0c7799d48ade7aa11357cb72205391da7502
Zhipeng Dong <DrawZeroPoint@users.noreply.github.com>
2020-06-03 20:30:58 +0800

    refine format

--- rotools/interface/moveit.py
+++ rotools/interface/moveit.py
@@ -1,11 +1,15 @@
 from __future__ import print_function
 
 import sys
-import rospy
-import moveit_commander
 
-import geometry_msgs.msg as GeometryMsg
-import moveit_msgs.msg as MoveItMsg
+try:
+    import rospy
+    import moveit_commander
+
+    import geometry_msgs.msg as GeometryMsg
+    import moveit_msgs.msg as MoveItMsg
+except ModuleNotFoundError:
+    pass
 
 from rotools.utility import common
 
@@ -67,21 +71,21 @@ class MoveGroupInterface(object):
         return self.move_group.get_active_joints()
 
     def get_joint_state(self):
-        """
+        """Get joint states for the robot
 
         :return: List[float]
         """
         return self.move_group.get_current_joint_values()
 
     def get_current_pose(self):
-        """
+        """Get the eef pose in ROS format.
 
         :return: pose_stamped
         """
         return self.move_group.get_current_pose()
 
     def go_to_joint_state(self, joint_goal):
-        """
+        """Set the joint states as desired.
 
         :param joint_goal: joint state list
         :return:
@@ -96,10 +100,10 @@ class MoveGroupInterface(object):
         return common.all_close(joint_goal, current_joints, 0.01)
 
     def go_to_pose_goal(self, pose_goal):
-        """
+        """Set the eef pose as desired.
 
         :param pose_goal: geometry_msgs.msg.Pose()
-        :return:
+        :return: if in goal pose
         """
         self.move_group.set_pose_target(pose_goal)
 
@@ -113,45 +117,41 @@ class MoveGroupInterface(object):
         current_pose = self.move_group.get_current_pose().pose
         return common.all_close(pose_goal, current_pose, 0.01)
 
-    def plan_cartesian_path(self, scale=1):
-        waypoints = []
+    def plan_cartesian_path(self, waypoints):
+        """Given way points in a list of geometry msg pose, plan a path
+        go through all way points.
 
+        :param waypoints: List[Pose]
+        :param
+
+        example
         # wpose = self.move_group.get_current_pose().pose
-        # wpose.position.z -= scale * 0.1  # First move up (z)
-        # wpose.position.y += scale * 0.2  # and sideways (y)
+        # wpose.position.z -= 0.1  # First move up (z)
+        # wpose.position.y += 0.2  # and sideways (y)
         # waypoints.append(copy.deepcopy(wpose))
         #
-        # wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+        # wpose.position.x += 0.1  # Second move forward/backwards in (x)
         # waypoints.append(copy.deepcopy(wpose))
         #
-        # wpose.position.y -= scale * 0.1  # Third move sideways (y)
+        # wpose.position.y -= 0.1  # Third move sideways (y)
         # waypoints.append(copy.deepcopy(wpose))
-        #
-        # # We want the Cartesian path to be interpolated at a resolution of 1 cm
-        # # which is why we will specify 0.01 as the eef_step in Cartesian
-        # # translation.  We will disable the jump threshold by setting it to 0.0,
-        # # ignoring the check for infeasible jumps in joint space, which is sufficient
-        # # for this tutorial.
-        # (plan, fraction) = move_group.compute_cartesian_path(
-        #     waypoints,  # waypoints to follow
-        #     0.01,  # eef_step
-        #     0.0)  # jump_threshold
-        #
-        # # Note: We are just planning, not asking move_group to actually move the robot yet:
-        # return plan, fraction
+        """
+
+        (plan, fraction) = self.move_group.compute_cartesian_path(
+            waypoints,  # waypoints to follow
+            0.01,  # eef_step
+            0.0)  # jump_threshold
+
+        # Note: We are just planning, not asking move_group to actually move the robot yet:
+        return plan, fraction
 
     def display_trajectory(self, plan):
-        ## BEGIN_SUB_TUTORIAL display_trajectory
-        ##
-        ## Displaying a Trajectory
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-        ## group.plan() method does this automatically so this is not that useful
-        ## here (it just displays the same trajectory again):
-        ##
-        ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-        ## We populate the trajectory_start with our current robot state to copy over
-        ## any AttachedCollisionObjects and add our plan to the trajectory.
+        # You can ask RViz to visualize a plan (aka trajectory) for you. But the
+        # group.plan() method does this automatically so this is not that useful
+        # here (it just displays the same trajectory again):
+        # A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
+        # We populate the trajectory_start with our current robot state to copy over
+        # any AttachedCollisionObjects and add our plan to the trajectory.
         display_trajectory = MoveItMsg.DisplayTrajectory()
         display_trajectory.trajectory_start = self.commander.get_current_state()
         display_trajectory.trajectory.append(plan)
@@ -162,17 +162,13 @@ class MoveGroupInterface(object):
         self.move_group.execute(plan, wait=True)
 
     def wait_for_state_update(self, name_is_known=False, obj_is_attached=False, timeout=4):
-        ## BEGIN_SUB_TUTORIAL wait_for_scene_update
-        ##
-        ## Ensuring Collision Updates Are Received
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## If the Python node dies before publishing a collision object update message, the message
-        ## could get lost and the box will not appear. To ensure that the updates are
-        ## made, we wait until we see the changes reflected in the
-        ## ``get_attached_objects()`` and ``get_known_object_names()`` lists.
-        ## For the purpose of this tutorial, we call this function after adding,
-        ## removing, attaching or detaching an object in the planning scene. We then wait
-        ## until the updates have been made or ``timeout`` seconds have passed
+        # If the Python node dies before publishing a collision object update message, the message
+        # could get lost and the box will not appear. To ensure that the updates are
+        # made, we wait until we see the changes reflected in the
+        # ``get_attached_objects()`` and ``get_known_object_names()`` lists.
+        # For the purpose of this tutorial, we call this function after adding,
+        # removing, attaching or detaching an object in the planning scene. We then wait
+        # until the updates have been made or ``timeout`` seconds have passed
         start = rospy.get_time()
         seconds = rospy.get_time()
         while (seconds - start < timeout) and not rospy.is_shutdown():
@@ -202,11 +198,7 @@ class MoveGroupInterface(object):
         box_name = self.attached_object_name
         scene = self.scene
 
-        ## BEGIN_SUB_TUTORIAL add_box
-        ##
-        ## Adding Objects to the Planning Scene
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## First, we will create a box in the planning scene at the location of the left finger:
+        # First, we will create a box in the planning scene at the location of the left finger:
         box_pose = GeometryMsg.PoseStamped()
         box_pose.header.frame_id = "panda_leftfinger"
         box_pose.pose.orientation.w = 1.0
@@ -214,7 +206,6 @@ class MoveGroupInterface(object):
         box_name = "box"
         scene.add_box(box_name, box_pose, size=(0.1, 0.1, 0.1))
 
-        ## END_SUB_TUTORIAL
         # Copy local variables back to class variables. In practice, you should use the class
         # variables directly unless you have a good reason not to.
         self.attached_object_name = box_name
@@ -243,20 +234,15 @@ class MoveGroupInterface(object):
         eef_link = self.eef_link
         group_names = self.group_names
 
-        ## BEGIN_SUB_TUTORIAL attach_object
-        ##
-        ## Attaching Objects to the Robot
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## Next, we will attach the box to the Panda wrist. Manipulating objects requires the
-        ## robot be able to touch them without the planning scene reporting the contact as a
-        ## collision. By adding link names to the ``touch_links`` array, we are telling the
-        ## planning scene to ignore collisions between those links and the box. For the Panda
-        ## robot, we set ``grasping_group = 'hand'``. If you are using a different robot,
-        ## you should change this value to the name of your end effector group name.
+        # Next, we will attach the box to the Panda wrist. Manipulating objects requires the
+        # robot be able to touch them without the planning scene reporting the contact as a
+        # collision. By adding link names to the ``touch_links`` array, we are telling the
+        # planning scene to ignore collisions between those links and the box. For the Panda
+        # robot, we set ``grasping_group = 'hand'``. If you are using a different robot,
+        # you should change this value to the name of your end effector group name.
         grasping_group = 'hand'
         touch_links = robot.get_link_names(group=grasping_group)
         scene.attach_box(eef_link, box_name, touch_links=touch_links)
-        ## END_SUB_TUTORIAL
 
         # We wait for the planning scene to update.
         return self.wait_for_state_update(obj_is_attached=True, name_is_known=False, timeout=timeout)
@@ -269,13 +255,8 @@ class MoveGroupInterface(object):
         scene = self.scene
         eef_link = self.eef_link
 
-        ## BEGIN_SUB_TUTORIAL detach_object
-        ##
-        ## Detaching Objects from the Robot
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## We can also detach and remove the object from the planning scene:
+        # We can also detach and remove the object from the planning scene:
         scene.remove_attached_object(eef_link, name=box_name)
-        ## END_SUB_TUTORIAL
 
         # We wait for the planning scene to update.
         return self.wait_for_state_update(name_is_known=True, obj_is_attached=False, timeout=timeout)
